5f27eafbc316cf3cbf7f957d5245d21ec006adcf
vivian <undertalefan812@gmail.com>
2026-07-24 16:37:00 -0400

    add prototype to is_valid

--- year_2023/src/days/day3.rs
+++ year_2023/src/days/day3.rs
@@ -1,6 +1,11 @@
+
+fn is_valid(row: usize, column: usize,row_size: usize, column_size: usize) -> bool {
+    todo!()
+}
+
 pub fn solve() {
     println!("Test");
-    let input = "\
+    let input: Vec<Vec<char>> = "\
     467..114..
     ...*......
     ..35..633.
@@ -10,5 +15,5 @@ pub fn solve() {
     ..592.....
     ......755.
     ...$.*....
-    .664.598..";
+    .664.598..".lines().map(|line| line.chars().collect()).collect();
 }
